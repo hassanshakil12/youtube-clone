7d683c156455c1e989d6ee3174a7c25ee65a0043
Muhammad Hassan <hassanshakil12@gmail.com>
2024-04-01 17:46:07 +0500

    Initialization of repository

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import { Stack } from "@mui/material";
+import { Link } from "react-router-dom";
+import { logo } from "../utils/constant";
+import SearchBar from "./SearchBar";
+
+const Navbar = () => (
+  <Stack
+    direction="row"
+    p={2}
+    alignItems="center"
+    sx={{
+      position: "sticky",
+      backgroundColor: "#000",
+      top: 0,
+      justifyContent: "space-between",
+    }}
+  >
+    <Link to="/" style={{ display: "flex", alignItems: "center" }}>
+      <img src={logo} alt="logo" height={45} />
+    </Link>
+    <SearchBar />
+  </Stack>
+);
+
+export default Navbar;
---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+export const BASE_URL = "https://youtube-v31.p.rapidapi.com";
+
+const options = {
+  params: {
+    maxResults: "50",
+  },
+  headers: {
+    "X-RapidAPI-Key": "33311ae8c5msh8c24e0fc30174e2p1582d4jsn0ff789256996",
+    "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  try {
+    const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+    return data;
+  } catch (error) {
+    console.error(`Data Fetching Error: ${error}`);
+  }
+};
