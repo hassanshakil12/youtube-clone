d20bd09e3b882a7a635abb4d468360313b78d553
Muhammad Hassan <hassanshakil12@gmail.com>
2024-04-04 20:01:58 +0500

    Domain

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const app = () => (
     <Box sx={{ backgroundColor: "#000" }}>
       <Navbar />
       <Routes>
-        <Route path="/" exact element={<Feed />} />
+        <Route path="youtube-clone/" exact element={<Feed />} />
         <Route path="/video/:id" element={<VideoDetail />} />
         <Route path="/channel/:id" element={<ChannelDetail />} />
         <Route path="/search/:searchTerm" element={<SearchFeed />} />
