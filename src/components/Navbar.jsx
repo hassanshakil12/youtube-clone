1037c18d163b9ae056c4176648b80cb1e713facc
Muhammad Hassan <hassanshakil12@gmail.com>
2024-04-04 20:29:45 +0500

    Routing fixes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => (
       justifyContent: "space-between",
     }}
   >
-    <Link to="/" style={{ display: "flex", alignItems: "center" }}>
+    <Link to="youtube-clone/" style={{ display: "flex", alignItems: "center" }}>
       <img src={logo} alt="logo" height={45} />
     </Link>
     <SearchBar />
